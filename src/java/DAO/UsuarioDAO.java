87cb493e00f6335e9dfe458bac5c4750a83b5b6c
feernando-cortes <fercho_cuatee@hotmail.com>
2014-08-10 14:12:30 -0500

    Se agrego UsuarioDAO con su metodo agregarUsuario() y query, tambien se agrego un Web Service con su operacion(metodo) para agregarUsuario()

---
+++ src/java/DAO/UsuarioDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+import Utilerias.ConexionSQLServer;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Feer
+ */
+public class UsuarioDAO {
+    
+    String registrar = "insert into usuario(Usuario, Contrasena, idTipoUsuario)" +
+                       "values(?,?,?);";
+
+    public boolean insertarUsuario(String usuario, String contrasena, String idTipoUsuario) {
+        boolean status = false;
+        try {
+            Connection con = ConexionSQLServer.getConnection();
+            PreparedStatement ps = con.prepareStatement(registrar);
+
+            ps.setString(1, usuario);
+            ps.setString(2, contrasena);
+            ps.setString(3, idTipoUsuario);           
+
+            status = ps.executeUpdate() == 1;
+            ps.close();
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return status;
+    }    
+}
---
+++ src/java/SERVICE/Service.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SERVICE;
+
+import DAO.UsuarioDAO;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Feer
+ */
+@WebService(serviceName = "Service")
+public class Service {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "agregarUsuario")
+    public String agregarUsuario(@WebParam(name = "usuario") String usuario, @WebParam(name = "contrasena") String contrasena, @WebParam(name = "idTipoUsuario") String idTipoUsuario) {
+        //TODO write your implementation code here:
+        UsuarioDAO dao = new UsuarioDAO();
+        String respuesta = "false";
+        
+        if(dao.insertarUsuario(usuario, contrasena, idTipoUsuario)){
+            respuesta = "true";
+        }        
+        return respuesta;
+    }
+}
